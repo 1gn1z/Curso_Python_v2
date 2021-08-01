49689ccffec942e5283c23811084a12437f2a39f
1gn1z_el_angel <kebor327@gmail.com>
2021-08-01 01:25:08 -0500

    ex4.5. 1. programa listo :3

---
+++ ex4.5_area_triangulo.py
@@ -0,0 +1,10 @@
+'''
+Ejercicio 4.5: Calcular el área de un triángulo dadas la base y la altura
+'''
+
+base = int(input('Digite la base: '))
+altura = int(input('Digite la altura: '))
+
+area = (base * altura) /2
+print(area)
+
