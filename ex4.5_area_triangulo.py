944d03f3a733b545b10b51e09dde2c70e816bf78
1gn1z_el_angel <kebor327@gmail.com>
2021-08-01 01:27:59 -0500

    ex4.5. 2. implementacion de una funcion

--- ex4.5_area_triangulo.py
+++ ex4.5_area_triangulo.py
@@ -1,10 +1,21 @@
 '''
 Ejercicio 4.5: Calcular el área de un triángulo dadas la base y la altura
-'''
+
 
 base = int(input('Digite la base: '))
 altura = int(input('Digite la altura: '))
 
 area = (base * altura) /2
 print(area)
+'''
+# Vamos a intentar usar una función:
 
+def area_triangulo(base, altura):
+    area = base * altura / 2
+    return area
+
+base = int(input('Digite la base: '))
+altura = int(input('Digite la altura: '))
+
+area_total = area_triangulo(base, altura)
+print(area_total)
