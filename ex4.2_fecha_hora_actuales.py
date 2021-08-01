cdc0d7f70f275274456c700c09986cfaf77796a3
1gn1z_el_angel <kebor327@gmail.com>
2021-07-31 21:52:18 -0500

    ex4.2, 2o, fecha y hora formateadas con strftime

--- ex4.2_fecha_hora_actuales.py
+++ ex4.2_fecha_hora_actuales.py
@@ -5,3 +5,7 @@ Ejercicio 4.2: Mostrar en pantalla la fecha y hora actuales del sistema.
 import datetime
 
 ahora = datetime.datetime.now()
+print('Fecha y hora actuales:',ahora)
+
+ahora_formato = ahora.strftime('%H:%M:%S %Y-%m-%d')
+print(ahora_formato)
