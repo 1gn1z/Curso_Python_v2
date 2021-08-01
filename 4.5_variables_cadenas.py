2c5296d02f6a6f58f2a4d27dc935a5535dfe6654
1gn1z_el_angel <kebor327@gmail.com>
2021-07-31 20:53:15 -0500

    4.5 4o commit, mostramos el tipo de dato de las variables

--- 4.5_variables_cadenas.py
+++ 4.5_variables_cadenas.py
@@ -6,3 +6,9 @@ nombre_completo = nombre + ' ' + apellido
 print('Nombre: ', nombre)
 print('Apellido: ', apellido)
 print('Nombre completo: ', nombre_completo)
+
+print()
+
+print('Tipo de dato de la variable nombre:', type(nombre))
+print('Tipo de dato de la variable apellido:', type(apellido))
+print('Tipo de dato de la variable nombre_completo:', type(nombre_completo))
