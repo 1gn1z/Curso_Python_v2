6109d680735171df147545b54771c9c27ff021f9
1gn1z_el_angel <kebor327@gmail.com>
2021-08-01 01:34:02 -0500

    ex4.5. 3. cambio de int a float para evitar ValueError

--- ex4.5_area_triangulo.py
+++ ex4.5_area_triangulo.py
@@ -1,21 +1,21 @@
 '''
 Ejercicio 4.5: Calcular el área de un triángulo dadas la base y la altura
+'''
 
-
-base = int(input('Digite la base: '))
-altura = int(input('Digite la altura: '))
+base = float(input('Digite la base: '))
+altura = float(input('Digite la altura: '))
 
 area = (base * altura) /2
 print(area)
-'''
+
 # Vamos a intentar usar una función:
 
 def area_triangulo(base, altura):
     area = base * altura / 2
     return area
 
-base = int(input('Digite la base: '))
-altura = int(input('Digite la altura: '))
+base = float(input('Digite la base: '))
+altura = float(input('Digite la altura: '))
 
 area_total = area_triangulo(base, altura)
-print(area_total)
+print(f'El área del triángulo es igual a {area_total}')
