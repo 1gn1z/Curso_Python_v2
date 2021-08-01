77464e3d2b76546d5f60425a91e4239c0a7dd515
1gn1z_el_angel <kebor327@gmail.com>
2021-07-31 20:58:04 -0500

    4.6 1er commit, concatenación incorrecta

---
+++ 4.6_variables_concatenacion_intstr.py
@@ -0,0 +1,20 @@
+nombre = 'Kevor'
+apellido = 'Mastah'
+
+nombre_completo = nombre + ' ' + apellido
+
+print('Nombre: ', nombre)
+print('Apellido: ', apellido)
+print('Nombre completo: ', nombre_completo)
+
+print()
+
+print('Tipo de dato de la variable nombre:', type(nombre))
+print('Tipo de dato de la variable apellido:', type(apellido))
+print('Tipo de dato de la variable nombre_completo:', type(nombre_completo))
+
+print()
+
+edad = 28
+
+nombre_completo = nombre_completo + 'tiene ' + edad + 'años.'
