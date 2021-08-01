186cc83374dfe68e4615ab9e6605ba8e6a197ba7
1gn1z_el_angel <kebor327@gmail.com>
2021-07-31 20:38:32 -0500

    4.4 3er commit, mostrar ubicación de memoria de las variables

--- 4.4_tipo_de_dato.py
+++ 4.4_tipo_de_dato.py
@@ -9,3 +9,7 @@ print(dias_vividos)
 print('Tipo de dato de la variable edad:', type(edad))
 print('Tipo de dato de la variable dias_vividos:', type(dias_vividos))
 
+print()
+
+print('Posición en memoria de la variable edad:', id(edad))
+print('Posición en memoria de la variable dias_vividos:', id(dias_vividos))
