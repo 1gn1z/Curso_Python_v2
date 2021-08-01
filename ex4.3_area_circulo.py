b99fed1ec6ac6d85fb7676391ceed25787fe3478
1gn1z_el_angel <kebor327@gmail.com>
2021-08-01 01:00:14 -0500

    ex4.3, 1er, mi codigo sin ver el tutorial

---
+++ ex4.3_area_circulo.py
@@ -0,0 +1,10 @@
+'''
+Ejercicio 4.3: Solicitar al usuario el radio de un círculo y calcular su área.
+Fórmula: π * r2
+'''
+import math
+
+radio = int(input('Ingrese el radio del círculo: '))
+
+area = math.pi * radio * radio 
+print(area)
