d3856b799269231b123e6c9f56981ac761bbe673
1gn1z_el_angel <kebor327@gmail.com>
2021-07-31 20:35:05 -0500

    4.4 2o commit, cadena de texto que indica el tipo de dato

--- 4.4_tipo_de_dato.py
+++ 4.4_tipo_de_dato.py
@@ -6,5 +6,6 @@ dias_vividos = edad * 365
 print(edad)
 print(dias_vividos)
 
-print(type(edad))
-print(type(dias_vividos))
+print('Tipo de dato de la variable edad:', type(edad))
+print('Tipo de dato de la variable dias_vividos:', type(dias_vividos))
+
